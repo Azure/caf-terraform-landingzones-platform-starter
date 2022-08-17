3f46ee1212b44d1cc8f7bd18822beda77e916e2d
lolorol <laurent.lesle@gmail.com>
2022-08-17 12:41:18 +0800

    Update init script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,7 +6,11 @@ if [ "${CODESPACES}" = "true" ]; then
     git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
 fi
 
-sudo chmod 666 /var/run/docker.sock || true
+if [ -d "/var/run/docker.sock" ]; then
+  # Grant access to the docker socket
+  sudo chmod 666 /var/run/docker.sock
+fi
+
 sudo cp -R /tmp/.ssh-localhost/* ~/.ssh
 sudo chown -R $(whoami):$(whoami) ~ || true ?>/dev/null
 sudo chmod 400 ~/.ssh/*
@@ -21,8 +25,4 @@ git config --global --add safe.directory /tf/caf/landingzones/aztfmod
 git config --global --add safe.directory /tf/caf/aztfmod
 
 git config pull.rebase false 
-
-if [ ! -d /tf/caf/landingzones ]; then
-  git clone --branch int-5.6.0 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
-  sudo chmod +x /tf/caf/landingzones/templates/**/*.sh
-fi
+git submodule update --init --recursive
